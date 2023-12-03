6aad5d845cdd4a871d7e404620067412bf15d3b5
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-12-02 16:54:34 -0800

    [adventofcode] day1 solution

---
+++ python/day1/trebuchet/trebuchet.py
@@ -0,0 +1,29 @@
+def main():
+    print(calibrationDataTotal())
+
+def calibrationDataTotal():
+    """
+    Calibration Data Count:
+    Function read the file calibrations.txt and get the count
+    """
+    with open('calibrations.txt', 'r') as file:
+        data = file.readlines()
+    
+    count = 0
+
+    for d in data:
+        record = []
+        
+        for i in d.strip('\n'):
+            if i.isnumeric():
+                record.append(i)
+        if len(record) > 1:
+            count += int(f"{record[0]}{record[-1]}")
+        elif len(record) == 1:
+            count += int(f"{record[0]}{record[0]}")
+    return count
+
+        
+
+if __name__ == '__main__':
+    main()
