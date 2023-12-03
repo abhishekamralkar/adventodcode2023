9a2b89dfbca7a5ce11f666430e7f9fb3e2645416
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-12-03 14:54:55 -0800

    [adventofcode] day1 fix the test

--- python/day1/trebuchet/trebuchet_test.py
+++ python/day1/trebuchet/trebuchet_test.py
@@ -1,21 +1,11 @@
 from trebuchet import calibrationDataTotal
 import unittest
 
-# def test_calibrationDataTotal():
-#     assert calibrationDataTotal() == 54573, "Should be 54473"
-
-# if __name__ == "__main__":
-#     test_calibrationDataTotal()
-#     print("Calibration Data Looks Good Now!")
-
-
-# import unittest
-
 
 class TestData(unittest.TestCase):
 
     def test_calibrationDataTotal(self):
-        self.assertEqual(calibrationDataTotal(), 54573, "Should be 54473")
+        self.assertEqual(calibrationDataTotal(), 54573, "Should be 54573")
 
 if __name__ == '__main__':
     unittest.main()
