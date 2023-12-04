a4ec555a925126c1c0cceae0917f9b387d316326
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-12-03 19:30:31 -0800

    [adventofcode] add day1 part 2 solution

--- python/day1/trebuchet/trebuchet.py
+++ python/day1/trebuchet/trebuchet.py
@@ -7,23 +7,25 @@ def calibrationData():
     Function read the file calibrations.txt and get the count
     """
     with open('input.txt', 'r') as file:
-        data = file.readlines()
+        data = file.read().strip()
     
     count = 0
+    count2 = 0
 
-    for d in data:
+    for d in data.split("\n"):
         record = []
-        
-        for i in d.strip('\n'):
-            if i.isnumeric():
-                record.append(i)
-        if len(record) > 1:
-            count += int(f"{record[0]}{record[-1]}")
-        elif len(record) == 1:
-            count += int(f"{record[0]}{record[0]}")
-    return count
+        record2 = []
+        for i, v in enumerate(d):
+            if v.isdigit():
+                record.append(v)
+                record2.append(v)
+            for j, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+                if d[i:].startswith(val):
+                    record2.append(str(j+1))
+        count += int(record[0]+record[-1])
+        count2 += int(record2[0]+record2[-1])
+    return count, count2
 
-        
 
 if __name__ == '__main__':
     main()
